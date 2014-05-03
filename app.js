158e4c7bcc05c699d81483609ab027d8273c84a3
eXtreme <jacek.jedrzejewski@gmail.com>
2014-05-03 21:23:39 +0200

    some initial work on the queue, mongodb connection, draft of json queue messages

--- app.js
+++ app.js
@@ -1,18 +1,57 @@
+var spawn = require('child_process').spawn;
+var open = require('amqplib').connect('amqp://localhost');
+var MongoClient = require('mongodb').MongoClient;
+
 var q = 'tasks';
 
-var open = require('amqplib').connect('amqp://localhost');
-
 // Consumer
-open.then(function(conn) {
-    var ok = conn.createChannel();
-    ok = ok.then(function(ch) {
-        ch.assertQueue(q);
-        ch.consume(q, function(msg) {
-            if (msg !== null) {
-                console.log(msg.content.toString());
-                ch.ack(msg);
-            }
+open.then(function (conn) {
+    MongoClient.connect('mongodb://127.0.0.1:27017/ivan', function(err, db) {
+        if (err) { throw err; }
+        
+        var ok = conn.createChannel();
+        ok = ok.then(function (ch) {
+            ch.assertQueue(q);
+            ch.consume(q, function (msg) {
+                if (msg !== null) {
+                    var jsonMsg = JSON.parse(msg.content.toString());
+
+                    db.collection('documents').findOne({ _id: new require('mongodb').ObjectID(jsonMsg.documentId) }, function(err, doc) {
+                        if (err) {
+                            console.warn(err);
+                            return false;
+                        }
+                        
+                        switch (jsonMsg.type) {
+                            case "testing":
+                                console.log(jsonMsg.payload.msg);
+                                break;
+                            case "process":
+                                switch (jsonMsg.payload.fileType) {
+                                    case "pdf":
+                                        var prc = spawn('pdftotext',  ['input.pdf', 'output.txt']);
+                                        break;
+                                    case "docx":
+                                        var prc = spawn('docx2txt.pl',  ['input.docx', 'output.txt']);
+                                        break;
+                                }
+                                prc.on('close', function (code) {
+                                    if (code == 0) {
+
+                                    } else {
+
+                                    }
+                                });
+                                break;
+                        }
+                    });
+
+                    
+
+                    ch.ack(msg);
+                }
+            });
         });
+        return ok;
     });
-    return ok;
 }).then(null, console.warn);
--- test.js
+++ test.js
@@ -1,13 +1,21 @@
-var q = 'tasks';
-
 var open = require('amqplib').connect('amqp://localhost');
 
+var q = 'tasks';
+
 // Publisher
-open.then(function(conn) {
+open.then(function (conn) {
     var ok = conn.createChannel();
-    ok = ok.then(function(ch) {
+    ok = ok.then(function (ch) {
+        var jsonStr = {
+            type: "testing",
+            documentId: "5365400451e28086b3135bad",
+            payload: {
+                msg: "hello world"
+            }
+        };
+        
         ch.assertQueue(q);
-        ch.sendToQueue(q, new Buffer('something to do'));
+        ch.sendToQueue(q, new Buffer(JSON.stringify(jsonStr)));
     });
     return ok;
 }).then(null, console.warn);
